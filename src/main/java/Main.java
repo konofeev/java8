3ab1130aab0a902f49a67f7ba9fb2ceaf7f36ffa
Конофеев Роман <Konofeev.R.Ru@sberbank.ru>
2019-04-23 13:43:57 +0300

    Template

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+    public static void main(String[] arguments) throws Exception
+    {
+        System.out.println("Новое в Java 8");
+    }                                 
+}
